15b0afdaae8e30616c51fd0172c4c30a39eb5586
Deepesh Vashistha <deepeshvashistha.dv@gmail.com>
2016-11-14 08:25:39 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,34 +1,3 @@
-/*
-function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
-
-$(function(){
-$('form').on('submit', function(e){
-	e.preventDefault();
-	var request = gapi.client.youtube.search.list({
-		part: "snippet",
-		type: "video",
-		q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-		maxResults: 3,
-		order: "viewCount",
-		publishedAfter: "2016-01-01T00:00:0Z"	
-	});
-	request.execute(function(response){
-	//	console.log(response);
-	var results = response.result;
-	$.each(results.items, function(index, item){
-		//console.log(item);
-	//		$('#results').append(item.id.videoId+" "+item.snippet.title+"<br>");
-	$.get("item.html" , function(data) {
-		$('#results').append(tplawesome(data, [{"title":item.snippet.title ,"videoid":item.videoId}]));
-	});
-
-	});
-
-	});
-});
-});
-*/
-
 // a template for loading the video files ...acc to title
 function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
 
@@ -66,7 +35,7 @@ function resetVideoHeight() {
 }
 
 function init() {
-	gapi.client.setApiKey('AIzaSyBs-bFz81nkNFE5VKInGfYSn9Mxo1YIdBM');
+	gapi.client.setApiKey('Your API key');
 gapi.client.load("youtube" , "v3" , function(){
 
 });
